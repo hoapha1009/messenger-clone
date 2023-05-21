85789da9a848574dd76cc27fd457a3784667aa03
HoangPham <hoangpm@greengroups.vn>
2023-05-21 15:55:02 +0700

    Feat: Settings feat, delete converstation feat

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -10,7 +10,7 @@ interface ButtonProps {
 	disabled?: boolean;
 }
 
-const Button = ({
+const Button: React.FC<ButtonProps> = ({
 	type = 'button',
 	fullWidth,
 	children,
@@ -18,14 +18,25 @@ const Button = ({
 	secondary,
 	danger,
 	disabled,
-}: ButtonProps) => {
+}) => {
 	return (
 		<button
 			onClick={onClick}
 			type={type}
 			disabled={disabled}
 			className={clsx(
-				`flex justify-center rounded-md px-3 py-2 text-sm font-semibold focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2`,
+				`
+        flex 
+        justify-center 
+        rounded-md 
+        px-3 
+        py-2 
+        text-sm 
+        font-semibold 
+        focus-visible:outline 
+        focus-visible:outline-2 
+        focus-visible:outline-offset-2 
+        `,
 				disabled && 'opacity-50 cursor-default',
 				fullWidth && 'w-full',
 				secondary ? 'text-gray-900' : 'text-white',
